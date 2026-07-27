6964acf77c6edd9e12180cf5dc2a95c352ee08ba
Ashish Sahu <ashishsahu0052@gmail.com>
2026-07-27 23:52:42 +0530

    pracitce

---
+++ ATranslation.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class ATranslation {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String s = sc.next();
+        String t = sc.next();
+
+        String rev = new StringBuilder(s).reverse().toString();
+
+        if (rev.equals(t))
+            System.out.println("YES");
+        else
+            System.out.println("NO");
+    }
+}
---
+++ hello.java
@@ -0,0 +1,31 @@
+/**
+ * This class performs basic arithmetic operations.
+ *
+ * @author Ashish
+ * @version 1.0
+ */
+public class hello
+ {
+
+    /**
+     * Adds two integers.
+     *
+     * @param a First number
+     * @param b Second number
+     * @return Sum of a and b
+     */
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    /**
+     * Subtracts two integers.
+     *
+     * @param a First number
+     * @param b Second number
+     * @return Difference of a and b
+     */
+    public int subtract(int a, int b) {
+        return a - b;
+    }
+}

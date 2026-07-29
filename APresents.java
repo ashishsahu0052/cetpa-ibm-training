b7bf1a5e18590fb6f7c889858669836247933bd6
Ashish Sahu <ashishsahu0052@gmail.com>
2026-07-29 23:36:43 +0530

    pracitce

---
+++ APresents.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class APresents {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        int[] ans = new int[n + 1];
+
+        for (int i = 1; i <= n; i++) {
+            int x = sc.nextInt();
+            ans[x] = i;
+        }
+
+        for (int i = 1; i <= n; i++) {
+            System.out.print(ans[i] + " ");
+        }
+    }
+}
